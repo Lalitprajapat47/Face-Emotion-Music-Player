73f3c6ddaa3536b074957ee32fbe2d44b7164096
lp7554499 <lp7554499@gmail.com>
2026-08-02 00:19:38 +0530

    refine expression detection thresholds for better accuracy

--- src/features/Expressions/components/FaceExpression.jsx
+++ src/features/Expressions/components/FaceExpression.jsx
@@ -85,9 +85,9 @@ export default function FaceExpression() {
 
         if (smileLeft > 0.5 && smileRight > 0.5) {
           currentExpression = "😄 Happy";
-        } else if (jawOpen > 0.6 && browUp > 0.5) {
+        } else if (jawOpen > 0.2 && browUp > 0.2) {
           currentExpression = "😲 Surprised";
-        } else if (frownLeft > 0.5 && frownRight > 0.5) {
+        } else if (frownLeft > 0.0001 && frownRight > 0.0001) {
           currentExpression = "😢 Sad";
         }
 
