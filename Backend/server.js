c123e0f0258ff3948118dd4c8d56351a3f5067ae
lp7554499 <lp7554499@gmail.com>
2026-08-03 14:12:15 +0530

    add authentication routes and controller

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const app = require('./src/app')
-const connectToDb = require('./config/database')
+const connectToDb = require('./src/config/database')
 const { configDotenv } = require('dotenv')
 
 connectToDb()
