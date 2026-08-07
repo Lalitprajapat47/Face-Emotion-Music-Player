46d1ef71783b9741fadecc38075ea4bdb7af59d4
lp7554499 <lp7554499@gmail.com>
2026-08-07 23:29:21 +0530

    Add song context provider and API service for song retrieval

--- Frontend/src/features/home/song.context.jsx
+++ Frontend/src/features/home/song.context.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+import { createContext, useState } from "react";
+
+export const songContext = createContext();
+
+export const SongContextProvider = ({ children }) => {
+  const [song, setSong] = useState(null);
+
+  return (
+    <songContext.Provider value={{ song, setSong }}>
+      {children}
+    </songContext.Provider>
+  );
+};
