b8e31295d924d2f518e68d91fd2bf11c364a8541
lp7554499 <lp7554499@gmail.com>
2026-08-07 21:28:40 +0530

    Add Home component and song context file

---
+++ Frontend/src/features/home/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FaceExpression from './../../Expressions/components/FaceExpression'
+
+const Home = () => {
+  return (
+    <FaceExpression/>
+  )
+}
+
+export default Home
